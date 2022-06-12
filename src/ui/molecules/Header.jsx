fa5fc6a0125aa2a81ad5c9eb096b5f2b57257d01
Jesus Calderin <jesuscalderin91@gmail.com>
2022-06-11 22:03:14 -0400

    feat: iconos

--- src/ui/molecules/Header.jsx
+++ src/ui/molecules/Header.jsx
@@ -1,8 +1,15 @@
+import { Crow } from "../atoms/Crow";
+import { Star } from "../atoms/Star";
+
 export const Header = ({ currentRecord, bestRecord }) => {
   return (
     <header className="header">
-      <div className="current-record">Record actual: {currentRecord}</div>
-      <div className="best-record">Mejor puntaje: {bestRecord}</div>
+      <div className="current-record">
+        <Star /> Record actual: {currentRecord}
+      </div>
+      <div className="best-record">
+        <Crow /> Mejor puntaje: {bestRecord}
+      </div>
     </header>
   );
 };
